8ed020987aeaf583256ffb66766018117488afc1
David Coles <coles.david@gmail.com>
2020-07-11 18:29:22 -0700

    Switch to python-nest for streaming API
    This avoids us needing to poll, as we can get updates as soon
    as they occur.

--- nest_prometheus_exporter.py
+++ nest_prometheus_exporter.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-
 """Prometheus Exporter for Nest Thermostat."""
 
 import asyncio
+import concurrent.futures
 import datetime
 import email
 import hashlib
@@ -10,115 +10,86 @@ import json
 import logging
 import os
 import sys
-from urllib.parse import urljoin, quote
 
-import aiohttp
 from aiohttp import web
 import prometheus_client
+import nest
 
 ACCESS_TOKEN = os.getenv('NEST_ACCESS_TOKEN')
-THERMOSTAT_ID = os.getenv('NEST_THERMOSTAT_ID')
 NEST_API = 'https://developer-api.nest.com'
 MIN_INTERVAL = datetime.timedelta(seconds=59)  # ~ 1 minute
 PROMETHEUS_PORT = 9111
 
 last_connection = prometheus_client.Gauge(
     'nest_last_connection',
-    'Unix timestamp (seconds) of the last successful interaction with the Nest service')
+    'Unix timestamp (seconds) of the last successful interaction with the Nest service',
+    ['thermostat_id'])
+is_online = prometheus_client.Gauge(
+    'nest_is_online',
+    'Device connection status with the Nest Service (1 for online, 0 for offline)',
+    ['thermostat_id'])
 ambient_temperature_c = prometheus_client.Gauge(
     'nest_ambient_temperature_c',
-    'Temperature, measured at the device, in half degrees Celsius (0.5°C)')
+    'Temperature, measured at the device, in half degrees Celsius (0.5°C)',
+    ['thermostat_id'])
 humidity = prometheus_client.Gauge(
     'nest_humidity',
-    'Humidity, in percent (%) format, measured at the device, rounded to the nearest 5%')
+    'Humidity, in percent (%) format, measured at the device, rounded to the nearest 5%',
+    ['thermostat_id'])
 heating = prometheus_client.Gauge(
     'nest_heating',
-    'Indicates whether HVAC system is actively heating')
+    'Indicates whether HVAC system is actively heating',
+    ['thermostat_id'])
 cooling = prometheus_client.Gauge(
     'nest_cooling',
-    'Indicates whether HVAC system is actively cooling')
+    'Indicates whether HVAC system is actively cooling',
+    ['thermostat_id'])
 target_temperature_high_c = prometheus_client.Gauge(
     'nest_target_temperature_high_c',
-    'Maximum target temperature, displayed in half degrees Celsius (0.5°C)')
+    'Maximum target temperature, displayed in half degrees Celsius (0.5°C)',
+    ['thermostat_id'])
 target_temperature_low_c = prometheus_client.Gauge(
     'nest_target_temperature_low_c',
-    'Minimum target temperature, displayed in half degrees Celsius (0.5°C)')
+    'Minimum target temperature, displayed in half degrees Celsius (0.5°C)',
+    ['thermostat_id'])
+time_to_target = prometheus_client.Gauge(
+    'nest_time_to_target',
+    'The time, in minutes, that it will take for the structure to reach the target temperature',
+    ['thermostat_id'])
 
 
-def update_metrics(state):
-    """Update Prometheus metrics."""
-    last_connection.set(parse_timestamp(state['last_connection']).timestamp())
-    ambient_temperature_c.set(state['ambient_temperature_c'])
-    humidity.set(state['humidity'])
-    heating.set(1 if state['hvac_state'] == 'heating' else 0)
-    cooling.set(1 if state['hvac_state'] == 'cooling' else 0)
-    target_temperature_high_c.set(
-        state['target_temperature_high_c'] if state['hvac_mode'] == 'heat-cool'
-        else state['target_temperature_c'] if state['hvac_mode'] == 'cool'
-        else float('nan'))
-    target_temperature_low_c.set(
-        state['target_temperature_low_c'] if state['hvac_mode'] == 'heat-cool'
-        else state['target_temperature_c'] if state['hvac_mode'] == 'heat'
-        else float('nan'))
+def update_thermostat_metrics(thermostat: nest.nest.Thermostat):
+    """Update Prometheus thermostat metrics."""
+    logging.debug('Updating %s (%s)', thermostat.name, thermostat.device_id)
+
+    id = thermostat.device_id
+    device = thermostat._device  # for getting temperate in deg. C
+
+    c = device['ambient_temperature_c']
+    f = device['ambient_temperature_f']
+    logging.debug('Temperature %.1f°C, %.0f°F (%.1f°C)', c, f, c_to_f(f))
+
+    last_connection.labels(thermostat_id=id).set(
+        parse_timestamp(thermostat.last_connection).timestamp())
+    is_online.labels(thermostat_id=id).set(1 if thermostat.online else 0)
+
+    ambient_temperature_c.labels(thermostat_id=id).set(device['ambient_temperature_c'])
+    humidity.labels(thermostat_id=id).set(thermostat.humidity)
+    heating.labels(thermostat_id=id).set(1 if thermostat.hvac_state == 'heating' else 0)
+    cooling.labels(thermostat_id=id).set(1 if thermostat.hvac_state == 'cooling' else 0)
+    target_temperature_high_c.labels(thermostat_id=id).set(
+        device['target_temperature_high_c'])
+    target_temperature_low_c.labels(thermostat_id=id).set(
+        device['target_temperature_low_c'])
+    time_to_target.labels(thermostat_id=id).set(int(thermostat.time_to_target[1:]))
+
+
+def c_to_f(temp_f):
+    return (temp_f - 32) / 1.8
 
 
 def parse_timestamp(s: str) -> datetime.datetime:
     return datetime.datetime.strptime(s, '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=datetime.timezone.utc)
-
-
-class ThermostatCollector:
-    def __init__(self, thermostat_id: str, access_token: str):
-        self.thermostat_id = thermostat_id
-        self.access_token = access_token
-
-        self._lock = asyncio.Lock()
-        self._state = {}
-        self._last_updated = None
-
-    async def get_latest_state(self):
-        """Get latest thermostat state.
-
-        If called more frequently than once per minute, this will return cached data.
-        """
-        async with self._lock:
-            now = datetime.datetime.now(tz=datetime.timezone.utc)
-            if self._last_updated and self._last_updated + MIN_INTERVAL > now:
-                # Already updated recently
-                return self._state, self._last_updated
-
-            logging.info('Updating thermostat state (last updated: %s)', self._last_updated)
-            headers = {'Authorization': f'Bearer {self.access_token}'}
-            url = urljoin(NEST_API, f'/devices/thermostats/{quote(THERMOSTAT_ID)}')
-
-            async with aiohttp.ClientSession(headers=headers) as session:
-                resp = await get(session, url)
-
-            self._state = await resp.json()
-            self._last_updated = datetime.datetime.now(tz=datetime.timezone.utc)
-
-            return self._state, self._last_updated
-
-
-async def get(session: aiohttp.ClientSession, url: str, max_redirects=10):
-    """GET URL, following any redirects, including session headers."""
-    request_info = None
-    history = []
-
-    for _ in range(max_redirects):
-        resp = await session.get(url, allow_redirects=False)
-        request_info = resp.request_info
-        history.append(resp)
-        resp.raise_for_status()
-
-        if resp.status // 100 != 3:  # 3xx
-            break
-
-        url = resp.headers['Location']
-        history.append(url)
-    else:
-        raise aiohttp.TooManyRedirects(request_info, tuple(history))
-
-    return resp
 
 
 def last_modified_headers(dt: datetime.datetime) -> dict:
@@ -129,37 +100,43 @@ def last_modified_headers(dt: datetime.datetime) -> dict:
     }
 
 
-def main():
+async def main():
     logging.basicConfig(level=logging.DEBUG)
 
     if not ACCESS_TOKEN:
         logging.error('NEST_ACCESS_TOKEN not found in environment')
         sys.exit(1)
 
-    if not THERMOSTAT_ID:
-        logging.error('NEST_THERMOSTAT_ID not found in environment')
-        sys.exit(1)
-
     try:
         access_token_obj = json.loads(ACCESS_TOKEN)
-    except ValueError as e:
+        access_token = access_token_obj['access_token']
+    except (ValueError, KeyError) as e:
         logging.error('Failed to parse access token: %s', e)
         sys.exit(1)
 
+    napi = nest.Nest(access_token=access_token)
+    last_updated = None
+
+    def _update():
+        nonlocal last_updated
+
+        logging.debug('Updating...')
+        last_updated = datetime.datetime.now(tz=datetime.timezone.utc)
+        for t in napi.thermostats:
+            update_thermostat_metrics(t)
+
+    # Force initial update
+    _update()
+
     routes = web.RouteTableDef()
-    collector = ThermostatCollector(THERMOSTAT_ID, access_token_obj['access_token'])
 
-    @routes.get('/state')
-    async def _state(_: web.Response):
-        state, last_updated = await collector.get_latest_state()
-
-        return web.json_response(state, headers=last_modified_headers(last_updated))
+    @routes.get('/')
+    async def _index(_: web.Response):
+        html = '<a href="/metrics">Metrics</a>'
+        return web.Response(text=html, content_type='text/html')
 
     @routes.get('/metrics')
     async def _metrics(_: web.Request):
-        state, last_updated = await collector.get_latest_state()
-        update_metrics(state)
-
         body = prometheus_client.generate_latest()
         return web.Response(
             body=body,
@@ -169,8 +146,21 @@ def main():
 
     app = web.Application()
     app.add_routes(routes)
-    web.run_app(app, host='127.0.0.1', port=9101)
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    host, port = '127.0.0.1', 9101
+    logging.info('Listening on %s:%d', host, port)
+    site = web.TCPSite(runner, host, port)
+    await site.start()
+
+    loop = asyncio.get_event_loop()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as pool:
+        while True:
+            await loop.run_in_executor(pool, napi.update_event.wait)
+            napi.update_event.clear()
+            _update()
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
